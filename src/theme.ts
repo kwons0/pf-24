b4dca7d5341aabfd39126f3fefb0c305ee566c67
ksy1128 <ksy1128@echomarketing.co.kr>
2025-01-12 17:29:38 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,26 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#191A1D",
+    textColor: "#FFFFFF",
+    mainColor: "#1A73E8",
+
+    gray1: "#202124",
+    gray2: "#414141",
+    gray3: "#595959",
+    gray4: "#9A9A9A",
+    gray5: "#CDCDCD",
+}
+
+export const lightThemeTheme: DefaultTheme = {
+    bgColor: "#E6E6E6",
+    textColor: "#191A1D",
+    mainColor: "#1A73E8",
+
+    gray1: "#CDCDCD",
+    gray2: "#9A9A9A",
+    gray3: "#595959",
+    gray4: "#414141",
+    gray5: "#202124",
+}
+
