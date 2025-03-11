278202b2f3063cdf2cf9693d0509587ceb425925
ksy1128 <ksy1128@echomarketing.co.kr>
2025-03-11 19:10:31 +0900

    애니메이션 추가

--- src/Components/list.tsx
+++ src/Components/list.tsx
@@ -7,6 +7,7 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { isDarkAtom, isModalOpen } from "../atom";
 import { useEffect } from "react";
 import { breakpoints } from "../lib/constants";
+import { motion, transform } from "framer-motion";
 
 const mediaTablet = `@media (max-width: ${breakpoints.tablet})`;
 
@@ -19,15 +20,17 @@ const ProjectWrap = styled.div<IProjectPops>`
         ul{ order: 2; padding-left: 5%;}
         img{ order: 1;}
     }
-    img{width: 50%;}
 `
 
+const PjImg = styled(motion.img)`
+    width: 50%; cursor: pointer;
+`
 const PjTitle = styled.li<{isDark: boolean}>`
     p{ color: ${(props)=> props.theme.gray3 }; padding-bottom: 15px;}
     div{ font-size: 25px; font-weight: 600;}
 `
 const PjDesc = styled.li`margin: 30px 0 50px; width: 70%; line-height: 1.5; font-size:18px;`
-const PjBtn = styled.span<{isDark: boolean}>`
+const PjBtn = styled(motion.span)<{isDark: boolean}>`
     display: inline-block; padding: 10px 20px; line-height: 1; cursor: pointer; border-radius: 50px;
     background: ${({theme,isDark})=> isDark ? theme.gray2 : "#E5E5E5"};
 `
@@ -39,7 +42,12 @@ interface IProjectPops {
     layoutId: string
 }
 
-
+const hoverAnimation = {
+    hover : {
+        scale: 1.02, translateY: "-1%", translateX: "-1%",
+        transition: { type: "spring", damping: 15, mass: 0.5 }
+    } 
+}
 
 
 function List(){
@@ -79,11 +87,18 @@ function List(){
                                     <PjBtn 
                                         isDark={isDark}
                                         onClick={() => onProjectClick(v.href)}
+                                        variants={ {hover : { background: "#1A73E8", color: "#fff" }}} 
+                                        whileHover='hover'
                                     >view detail</PjBtn>
                                     { k == 0 ? <Deco src="asset/svg/deco_arrow1.svg" alt="deco"/> : null }
                                 </li>
                             </ul>
-                            <img src={"asset/intro/"+v.img+".png"} alt={v.img} onClick={() => onProjectClick(v.href)}/>
+                            <PjImg 
+                                src={"asset/intro/"+v.img+".png"} alt={v.img} 
+                                onClick={() => onProjectClick(v.href)}
+                                variants={hoverAnimation} 
+                                whileHover='hover'
+                            />
                         </ProjectWrap>
                     ))
                 }
